780326c7db0d05878ef6ec3ca62f337de78893b8
Miguel Fonseca <miguelcsf@gmail.com>
2011-12-08 15:06:16 +0000

    Simpler command parsing; forget regex.

--- commands.py
+++ commands.py
@@ -1,46 +1,70 @@
 #!/usr/bin/env python
 
-import re
-
-import event
+from event import Event
 from pdu import pdu
 from utils import log
 
 def update(args):
 
+    usage = 'Syntax: L1: Ai[-Fi.pdf]/Pi; [Aj/Pj; ...]'
+
     shop, queries = args.partition(':')[::2]
     if not queries:
-        log('Syntax: L1: Ai[-Fi.pdf]/Pi; [Aj/Pj; ...]')
+        log(usage)
         return
 
-    pat = ( r'\s*'
-        + r'(\w*)'          # Ai [match group 1]
-        + r'(-(\w*.\w*))?'  # [-i.pdf] (optional) [mg 2, 3]
-        + r'\s*/\s*'        # / (and whitespace)
-        + r'(\d*)'          # iPrice [mg 4]
-        + r'\s*;\s*'        # ; (and whitespace)
-    )
+    data = { 'shop' : shop, 'queries': [] }
 
-    data = []
-    for q in re.finditer(pat, queries):
-        item  = q.group(1)
-        fpath = q.group(3)
-        price = q.group(4)
+    queries = queries.split(';')
+    for q in queries:
+        if not q: break
+
+        item, price = q.strip().partition('/')[::2]
+        if not price:
+            log(usage)
+            log('Price missing for one or more items.')
+            return
+        try:
+            price = float(price)
+        except ValueError:
+            log(usage)
+            log('Item price must be a real number.')
+            return
+        item, fpath = item.partition('-')[::2]
         log('Read: UPDATE shop=%s item=%s fpath=%s price=%s'
                 % (shop, item, fpath, price))
-        data.append((shop, item, fpath, price))
+        data['queries'].append([item, fpath, price])
 
-    return event.Event(pdu.iUpdate, data)
+    return Event(pdu.iUpdate, data)
 
 
 def download(args):
-    log("Read: DOWNLOAD", args.split())
+
+    usage = 'Syntax: [L1[, L2...]]: A1[, A2...]'
+
+    shops, items = args.partition(':')[::2]
+
+    # Replacement needed in case there are no shops found
+    if not items:
+        shops, items = items, shops
+
+    if not items:
+        log(usage)
+        return
+
+    data = {
+        'shops': [s.strip() for s in shops.split(',')],
+        'items': [i.strip() for i in items.split(',')]
+    }
+
+    return Event(pdu.iDownload, data)
+
 
 def synch(args):
     log("Read: SYNCH", args.split())
 
 def quit(args):
-    log("Read: QUIT", args.split())
+    return Event(pdu.iQuit)
 
 commands = {
     'update'   : update,
