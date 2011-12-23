6bbad275bf4274d5077bb30823b4fe81a857abe4
João Ricardo Sares Teles de Matos <joao.matos@rnl.ist.utl.pt>
2011-12-23 20:22:31 +0000

    Class to read PDUs from sockets.

--- utils.py
+++ utils.py
@@ -8,3 +8,26 @@ def log(*msg):
     print(*msg, file=stderr)
 
 # vim: ts=8 et sw=4 sts=4 
+
+
+class pdu_reader():
+    def __init__(self, stream):
+        buffer=""
+        self.stream = stream
+        
+    def read(self):
+        """ Returns a PDU string from self.stream. """
+        # Find index of first occurence of \n in the buffer.
+                
+        # read from stream until we have a full PDU.
+        if self.buffer.find("\n") == -1:
+            tmp=""
+            while tmp.find("\n") == -1: 
+                tmp = self.stream.recv(1024)
+                self.buffer += tmp
+
+        i = self.buffer.find("\n")
+        pdu = self.buffer[:i]
+        self.buffer = self.buffer[i+1:]
+        return pdu
+        
